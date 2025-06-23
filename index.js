125947cac526c251370122cfcc28d58e456a32ef
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2025-06-23 21:58:36 +0200

    Fix getting folders

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.post('/get-all-categories', (req, res) => {
 app.post('/get-all-folders', (req, res) => {
     Database.GetAllFolders()
         .then((result) => {
-            res.status(200).json({ success: true, result })
+            res.status(200).json({ success: true, result: result });
         })
         .catch(err => {
             res.status(500).json({ error: 'Failed to get all folders' })
